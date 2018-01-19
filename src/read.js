3e59d44da5e96db2a7dfb6912636eeba1c6df4f8
Nektro <nektromc@gmail.com>
2018-01-18 21:22:19 -0500

    Add read(), a shorthand similar to fetch() for Blobs

---
+++ src/read.js
@@ -0,0 +1,43 @@
+/* jshint esversion:6 */
+
+const decoder = new TextDecoder();
+
+/**
+ * @param {Blob} blob
+ * @returns {Promise<ReadBody>}
+ */
+export function read(blob) {
+    const reader = new FileReader();
+    return new Promise(function(resolve, reject) {
+        reader.addEventListener('load', function(e) {
+            resolve(new ReadBody(e.target.result));
+        });
+        reader.readAsArrayBuffer(blob);
+    });
+}
+
+export class ReadBody {
+    /**
+     * @param {ArrayBuffer} ab 
+     */
+    constructor(ab) {
+        /** @constant */
+        this.buffer = ab;
+    }
+    /** @returns {Promise<ArrayBuffer>} */
+    async arrayBuffer() {
+        return this.buffer;
+    }
+    /** @returns {Promise<Blob>} */
+    async blob() {
+        return new Blob([this.buffer]);
+    }
+    /** @returns {Promise<Object>} */
+    async json() {
+        return JSON.parse(await this.text());
+    }
+    /** @returns {Promise<String>} */
+    async text() {
+        return decoder.decode(this.buffer);
+    }
+}
