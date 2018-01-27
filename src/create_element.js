4b3555330f250eee298979f7166020da9fc84077
Nektro <nektromc@gmail.com>
2018-01-26 21:49:21 -0500

    add create_element

---
+++ src/create_element.js
@@ -0,0 +1,17 @@
+//
+'use native';
+
+/**
+ * @param  {String} name
+ * @param  {Map<String,String>} attributes
+ * @param  {Array<Node>} children
+ * @param  {Map<String,Function<Event>>} events
+ * @return {HTMLElement}
+ */
+export function create_element(name, attributes, children, events) {
+    const element = document.createElement(name);
+    for (const attr of (attributes || new Map()).entries()) element.setAttribute(attr[0], attr[1]);
+    (children || []).forEach(v => element.appendChild(v));
+    for (const listener of (events || new Map()).entries()) element.addEventListener(listener[0], listener[1]);
+    return element;
+}
