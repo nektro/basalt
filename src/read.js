b49ec0f72865f267745335b74cc458c7e9b11c7c
Nektro <nektromc@gmail.com>
2018-02-07 23:41:50 -0500

    read :: update response to use native Response constructor

--- src/read.js
+++ src/read.js
@@ -6,40 +6,14 @@ const decoder = new TextDecoder();
 
 /**
  * @param {Blob} blob
- * @returns {Promise<ReadBody>}
+ * @returns {Promise<Response>}
  */
 export async function read(blob) {
     const reader = new FileReader();
     return new Promise(function(resolve, reject) {
         reader.addEventListener('load', function(e) {
-            resolve(new ReadBody(e.target.result));
+            resolve(new Response(e.target.result));
         });
         reader.readAsArrayBuffer(blob);
     });
-}
-
-export class ReadBody {
-    /**
-     * @param {ArrayBuffer} ab
-     */
-    constructor(ab) {
-        /** @constant */
-        this.buffer = ab;
-    }
-    /** @returns {Promise<ArrayBuffer>} */
-    async arrayBuffer() {
-        return this.buffer;
-    }
-    /** @returns {Promise<Blob>} */
-    async blob() {
-        return new Blob([this.buffer]);
-    }
-    /** @returns {Promise<Object>} */
-    async json() {
-        return JSON.parse(await this.text());
-    }
-    /** @returns {Promise<String>} */
-    async text() {
-        return decoder.decode(this.buffer);
-    }
 }
