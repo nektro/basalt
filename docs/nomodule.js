0cf9244dbe3899956e0ee95df0a0087ad475d261
Sean Denny <nektromc@gmail.com>
2018-02-09 14:24:59 -0500

    docs / add nomodule.js

---
+++ docs/nomodule.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    document.body.innerHTML += '<p>This browser does not support ES6 Modules</p>';
+    document.body.innerHTML += '<p>Consider upgrading to Edge 16+, Chrome 61+, Safari 10.1+, or iOS Safari 11+</p>'
+    document.body.innerHTML += '<p>See <a href="https://caniuse.com/#feat=es6-module">caniuse.com</a> more more info.</p>';
+    document.body.style.margin = '1em';
+    document.body.style.display = 'block';
+    document.querySelector('nav').style.display = 'none';
+    document.querySelector('main').style.display = 'none';
+    document.querySelector('footer').style.display = 'none';
+});
