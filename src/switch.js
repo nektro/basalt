439458f0544e4f9a505170b396dfb6f07257b7c7
Nektro <nektromc@gmail.com>
2018-03-11 22:55:39 -0400

    switch :: add switch module

---
+++ src/switch.js
@@ -0,0 +1,29 @@
+//
+'use strict';
+
+
+export default class Switch {
+    constructor() {
+        this._cases = new Map();
+        this._blocks = new Array();
+        this._def = null;
+    }
+    case(ca, cb) {
+        for (const c of ca) {
+            this._cases.set(c, this._blocks.length);
+        }
+        this._blocks.push(cb);
+    }
+    default(cb) {
+        this._def = cb;
+    }
+    run(arg, ...extras) {
+        if (this._cases.has(arg)) {
+            return this._blocks[this._cases.get(arg)](arg, ...extras);
+        }
+        else
+        if (this._def !== null) {
+            return this._def(arg, ...extras);
+        }
+    }
+}
