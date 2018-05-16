f495fd55df3ad4544792a7a3371abd3f8c072625
Sean Denny <nektromc@gmail.com>
2018-05-16 00:37:14 -0400

    create_element : use for...of for consistency

--- src/create_element.js
+++ src/create_element.js
@@ -12,7 +12,7 @@
 export function create_element(name, attributes, children, events) {
     const element = document.createElement(name);
     for (const attr of (attributes || new Map()).entries()) element.setAttribute(attr[0], attr[1]);
-    (children || []).forEach(v => element.appendChild(v));
+    for (const child of (children || [])) element.appendChild(child);
     for (const listener of (events || new Map()).entries()) element.addEventListener(listener[0], listener[1]);
     return element;
 }
