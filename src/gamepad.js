dd0f59198a06bc8f2991d3a9aedb7d3c12aa2ebd
Meghan <nektromc@gmail.com>
2018-07-09 16:55:39 -0400

    gamepad : add a button id reference for controller mappings

--- src/gamepad.js
+++ src/gamepad.js
@@ -74,3 +74,34 @@ function checkForGamepadData() {
 
 //
 requestAnimationFrame(checkForGamepadData);
+
+//
+export const Layouts = Object.freeze({
+    standard: {
+        axis: {
+            JOYSTICK_LEFT_X: 0,
+            JOYSTICK_LEFT_Y: 1,
+            JOYSTICK_RIGHT_X: 2,
+            JOYSTICK_RIGHT_Y: 3
+        },
+        button: {
+            A: 0,
+            B: 1,
+            X: 2,
+            Y: 3,
+            L1: 4,
+            R1: 5,
+            L2: 6,
+            R2: 7,
+            SELECT: 8,
+            START: 9,
+            L3: 10,
+            R3: 11,
+            DPAD_UP: 12,
+            DPAD_DOWN: 13,
+            DPAD_LEFT: 14,
+            DPAD_RIGHT: 15,
+            BUTTON_16: 16
+        }
+    }
+});
