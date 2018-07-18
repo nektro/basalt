68d81d5ee6193ec101abc122f509ee62c55262a7
Meghan <nektromc@gmail.com>
2018-07-18 16:04:58 -0400

    language/corgi : update corgi to use new language update

--- src/language/corgi.js
+++ src/language/corgi.js
@@ -8,6 +8,11 @@ import { Language } from "../language.js";
 import { ExpressionSimple, ExpressionContainer } from "../parse.js";
 
 
+//
+const nES = (...args) => new ExpressionSimple(...args);
+const nEC = (...args) => new ExpressionContainer(...args);
+
+//
 export default new (class extends Language {
     constructor() {
         super(
@@ -16,27 +21,25 @@ export default new (class extends Language {
             ["\""],
             true,
             true,
-            false
         );
 
-        const ELEMENTS = ["ElementI","ElementIA","ElementIC","ElementIAC"];
+        this.parser.addRule(["Id","S_=","String"], (list,i) => nEC("Attr", [list[i],list[i+2]]));
+        this.parser.addRule(["Attr"], (list, i) => nEC("AttrListPart", [list[i]]));
+        this.parser.addRule(["AttrListPart","S_,","AttrListPart"], (list, i) => nEC("AttrListPart", [...list[i].value,...list[i+2].value]));
+        this.parser.addRule(["S_[","AttrListPart","S_]"], (list, i) => nEC("AttrList", [...list[i+1].value]));
 
-        this.parser.addRule(["Identifier","Sym_=","String"], (list,i) => new ExpressionContainer("Attr", [list[i],list[i+2]]));
-        this.parser.addRule(["Attr"], (list, i) => new ExpressionContainer("AttrListPart", [list[i]]));
-        this.parser.addRule(["AttrListPart","Sym_,","AttrListPart"], (list, i) => new ExpressionContainer("AttrListPart", [...list[i].value,...list[i+2].value]));
-        this.parser.addRule(["Sym_[","AttrListPart","Sym_]"], (list, i) => new ExpressionContainer("AttrList", [...list[i+1].value]));
+        this.parser.addRule(["Id","AttrList"], (list,i) => nEC("ElementIA", [list[i],list[i+1]]));
+        this.parser.addRule(["S_(","ChildsPart"], (list, i) => nEC("Children", list[i+1].value));
+        this.parser.addRule(["String","S_)"], (list,i) => nEC("ChildsPart", [list[i]]));
+        this.parser.addRule(["Id","Children"], (list, i) => nEC("ElementIC", [list[i],list[i+1]]));
+        this.parser.addRule(["ElementIA","Children"], (list, i) => nEC("ElementIAC", [...list[i].value,list[i+1]]));
+        this.parser.addRule(["ELEMENTS","S_)"], (list, i) => nEC("ChildsPart", [list[i]]));
+        this.parser.addRule(["ELEMENTS","ChildsPart"], (list, i) => nEC("ChildsPart", [list[i],...list[i+1].value]));
+        this.parser.addRule(["String","ChildsPart"], (list, i) => nEC("ChildsPart", [list[i],...list[i+1].value]));
+        this.parser.addRule(["Id","ChildsPart"], (list, i) => nEC("ChildsPart", [nES("ElementI", [list[i], 0, 0,]),...list[i+1].value]));
+        this.parser.addRule(["Id","S_)"], (list, i) => nEC("ChildsPart", [nES("ElementI", [list[i], 0, 0,]),...list[i+1].value]));
 
-        this.parser.addRule(["Identifier","AttrList"], (list,i) => new ExpressionContainer("ElementIA", [list[i],list[i+1]]));
-        this.parser.addRule(["Sym_(","ChildsPart"], (list, i) => new ExpressionContainer("Children", list[i+1].value));
-        this.parser.addRule(["String","Sym_)"], (list,i) => new ExpressionContainer("ChildsPart", [list[i]]));
-        this.parser.addRule(["Identifier","Children"], (list, i) => new ExpressionContainer("ElementIC", [list[i],list[i+1]]));
-        this.parser.addRule(["ElementIA","Children"], (list, i) => new ExpressionContainer("ElementIAC", [...list[i].value,list[i+1]]));
-        this.parser.addRule([ELEMENTS,"Sym_)"], (list, i) => new ExpressionContainer("ChildsPart", [list[i]]));
-        this.parser.addRule([ELEMENTS,"ChildsPart"], (list, i) => new ExpressionContainer("ChildsPart", [list[i],...list[i+1].value]));
-        this.parser.addRule(["String","ChildsPart"], (list, i) => new ExpressionContainer("ChildsPart", [list[i],...list[i+1].value]));
-        this.parser.addRule(["Identifier","ChildsPart"], (list, i) => new ExpressionContainer("ChildsPart", [new ExpressionSimple("ElementI", 0, 0, [list[i]]),...list[i+1].value]));
-        this.parser.addRule(["Identifier","Sym_)"], (list, i) => new ExpressionContainer("ChildsPart", [new ExpressionSimple("ElementI", 0, 0, [list[i]]),...list[i+1].value]));
-
+        this.parser.addAlias("ELEMENTS", ["ElementI","ElementIA","ElementIC","ElementIAC"]);
     }
     isValidVarChar(c) {
         return ((c >= "a") && (c <= "z")) || ((c >= "0") && (c <= "9") || (c === "-"));
@@ -44,19 +47,21 @@ export default new (class extends Language {
     isValidIdentifier(word) {
         return true;
     }
-    compileExpression(ex) {
+    transform(ex) {
         switch (ex.type) {
-            case "Children": return ex.value.map(x => this.compileExpression(x)).join("");
-            case "ElementIAC": return `<${this.compileExpression(ex.value[0])}${this.compileExpression(ex.value[1])}>${this.compileExpression(ex.value[2])}</${this.compileExpression(ex.value[0])}>`;
-            case "Identifier": return ex.value;
-            case "AttrList": return ex.value.map(x => ` ${this.compileExpression(x)}`).join("");
-            case "Attr": return `${this.compileExpression(ex.value[0])}="${this.compileExpression(ex.value[1])}"`;
+            case "Children": return ex.value.map(x => this.transform(x)).join("");
+            case "ElementIAC": return `<${this.transform(ex.value[0])}${this.transform(ex.value[1])}>${this.transform(ex.value[2])}</${this.transform(ex.value[0])}>`;
+            case "Id": return ex.value;
+            case "AttrList": return ex.value.map(x => ` ${this.transform(x)}`).join("");
+            case "Attr": return `${this.transform(ex.value[0])}="${this.transform(ex.value[1])}"`;
             case "String": return ex.value.substring(1, ex.value.length - 1);
-            case "ElementIC": return `<${this.compileExpression(ex.value[0])}>${this.compileExpression(ex.value[1])}</${this.compileExpression(ex.value[0])}>`;
-            case "ElementIA": return `<${this.compileExpression(ex.value[0])}${this.compileExpression(ex.value[1])}></${this.compileExpression(ex.value[0])}>`;
-            case "ElementI": return `<${this.compileExpression(ex.value[0])}/>`
+            case "ElementIC": return `<${this.transform(ex.value[0])}>${this.transform(ex.value[1])}</${this.transform(ex.value[0])}>`;
+            case "ElementIA": return `<${this.transform(ex.value[0])}${this.transform(ex.value[1])}></${this.transform(ex.value[0])}>`;
+            case "ElementI": return `<${this.transform(ex.value[0])}/>`;
             //
-            default: console.warn(`no translation for ${ex.type}`, ex);
+            default:
+                console.warn(ex);
+                throw new Error(`no transformation for ${ex.type}`);
         }
     }
 })();
