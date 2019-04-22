38cb62dac0cfebe03aa884f57ca1406f256eb448
Meghan <nektromc@gmail.com>
2019-04-22 02:23:37 -0400

    file/torrent : replace "real" properties with getters in case values are missing

--- src/file/torrent.js
+++ src/file/torrent.js
@@ -17,18 +17,40 @@ import * as bencode from "./encoding/bencode.js";
 export function decode(ui8a) {
     return new (class Torrent {
         constructor() {
-            const properties = pipe(ui8a, bencode.decode);
-            this.announce = properties.get("announce");
-            this.announce_list = properties.get("announce-list").map(v => v[0]);
-            this.creation_date = properties.get("creation date");
-            this.comment = properties.get("comment");
-            this.created_by = properties.get("created by");
-            const info = properties.get("info");
-            this.files = info.get("files").map(v => [...v].reduce((ac,cv) => { ac[cv[0]] = cv[1]; return ac; }, {}));
-            this.name = info.get("name");
-            this.pieces = info.get("piece length");
-            this.piece_hashes = pipe(info.get("pieces").split(""), _array_group(20)).map(v => v.map(w => _char_to_hex(w)).reduce((c,v) => c+v, ""));
-            this.info_hash = pipe(info, bencode.encode, _string_to_uint8a, sha1);
+            this.properties = pipe(ui8a, bencode.decode);
+        }
+        get announce() {
+            return this.properties.get("announce");
+        }
+        get announce_list() {
+            return (this.properties.get("announce-list")||[]).map(v => v[0]);
+        }
+        get creation_date() {
+            return this.properties.get("creation date");
+        }
+        get comment() {
+            return this.properties.get("comment");
+        }
+        get created_by() {
+            return this.properties.get("created by");
+        }
+        get _info() {
+            return this.properties.get("info");
+        }
+        get files() {
+            return (this._info.get("files")||[]).map(v => [...v].reduce((ac,cv) => { ac[cv[0]] = cv[1]; return ac; }, {}));
+        }
+        get name() {
+            return this._info.get("name");
+        }
+        get pieces() {
+            return this._info.get("piece length");
+        }
+        get piece_hashes() {
+            return pipe((this._info.get("pieces")||"").split(""), _array_group(20)).map(v => v.map(w => _char_to_hex(w)).reduce((c,v) => c+v, ""));
+        }
+        get info_hash() {
+            return pipe(this._info, bencode.encode, _string_to_uint8a, sha1);
         }
         get magnet_link() {
             return `magnet:?xt=urn:btih:${this.info_hash}&dn=${encodeURIComponent(this.name)}${this.announce_list.reduce((ac,cv) => `${ac}&tr=${encodeURIComponent(cv)}`, "")}`;
